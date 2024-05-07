e4d7c96b13d660b32f87bb9e77daddfe8f7e9e68
masuda <r_masuda@emyus.jp>
2024-05-07 15:31:38 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/demo/Kensyu2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kensyu2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kensyu2Application.class, args);
+	}
+
+}
