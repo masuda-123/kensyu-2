852cf7868c9bc80b5979b08e2450d08ea5c38eae
masuda <r_masuda@emyus.jp>
2024-05-22 15:53:36 +0900

    user_lists画面を作成

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -44,5 +46,11 @@ public class UserService implements UserDetailsService {
 	@Transactional
 	public User findById(int userId) {
 		return userMapper.findById(userId);
+	}
+	
+	//ユーザーを全て取得
+	@Transactional
+	public ArrayList<User> findAll() {
+		return userMapper.findAll();
 	}
 }
